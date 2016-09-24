05ae62dc8edc8ff9a3f0f59bf6f2cadbec3d7cb2
HoangLanh <hoanglanhtb@gmail.com>
2016-09-25 01:07:12 +0700

    Crud category

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,51 @@
+class Admin::CategoriesController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    @categories = @categories.children_category.order("parent_id")
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = t "controllers.flash.common.create_success",
+        objects: t("activerecord.model.category")
+      redirect_to admin_categories_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = t "controllers.flash.common.update_success",
+        objects: t("activerecord.model.category")
+      redirect_to admin_categories_url
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @category && @category.destroy
+      flash[:success] = t "controllers.flash.common.destroy_success",
+        objects: t("activerecord.model.category")
+    else
+      flash[:danger] = t "controllers.flash.common.destroy_error",
+        objects: t("activerecord.model.category")
+    end
+    redirect_to admin_categories_url
+  end
+
+  private
+  def category_params
+    params.require(:category).permit :title, :parent_id
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
+
+  namespace :admin do
+    resources :categories, except: [:show]
+  end
 end
